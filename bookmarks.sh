0eddf3f4747064deed8f38e3efc3b097e6a7f73e
typegaro <lorenzo.di.giovanni00@gmail.com>
2025-06-05 15:21:04 +0200

    switch audio script

--- bookmarks.sh
+++ bookmarks.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 bookmark_dir=~/.scripts/bookmarks
 choice=$(cat $bookmark_dir | dmenu -p "Bookmark:" -l 10 -i)
+if [ -z "$choice" ]; then
+    exit 0
+fi
 xdotool type "$choice"
---
+++ switch-audio-output.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SINKS=$(pactl list short sinks | awk '{print $1 ":" $2}')
+
+CHOSEN=$(echo "$SINKS" | dmenu -i -p "audio output:")
+
+SINK_ID=$(echo "$CHOSEN" | cut -d ':' -f 1)
+
+if [ -n "$SINK_ID" ]; then
+    pactl set-default-sink "$SINK_ID"
+    
+    for INPUT in $(pactl list short sink-inputs | awk '{print $1}'); do
+        pactl move-sink-input "$INPUT" "$SINK_ID"
+    done
+fi
