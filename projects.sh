937574a4b82083531732a107cd191b82730e12d0
typegaro <lorenzo.di.giovanni00@gmail.com>
2025-08-09 10:56:58 +0200

    fix for projects

--- projects.sh
+++ projects.sh
@@ -1,10 +1,9 @@
-search_dirs=(~/Prj ~/Work ~/Work/backend ~/Amber)
+search_dirs=(~/Prj ~/Work ~/Work/backend ~/Work/sdk ~/Amber)
 dirs=( $(find "${search_dirs[@]}" -maxdepth 1 -type d) )
 path=$(printf "%s\n" "${dirs[@]}" | dmenu -l 10 -i)
 base=$(basename "$path")
 
 tmux new-session -c "$path" -s "$base" -d
-tmux send-keys -t "$base" "nvim \"$path\"" C-m
 
 notify-send "tmux session started" "Session: $base"
 if [ $(tmux ls 2>/dev/null | wc -l) -eq 1 ]; then
